00bec368c9816958f4c928204a797168b14422a2
mrtortuga <mrtortuga@ymail.com>
2017-08-01 22:31:46 -0400

    Added Main.functions

--- src/com/thomastri/Main.java
+++ src/com/thomastri/Main.java
@@ -2,9 +2,21 @@ package com.thomastri;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.Objects;
 
 public class Main {
+
+    public static void yell(String words) {
+
+        // requireNonNull is LAZY, which means it will only activate when triggered-
+        Objects.requireNonNull(words, () -> "Created issue" + Main.createIssue());
+        System.out.printf("%s!!! %n", words.toUpperCase());
+    }
+
+    private static String createIssue() {
+        System.out.println("Some external API call to a bug tracker");
+        return "#ABC!";
+    }
 
     public static void main(String[] args) {
         List<String> ingredients = Arrays.asList(
@@ -23,13 +35,14 @@ public class Main {
 //            }
 //        });
 
-        Consumer<String> printer = ingredient -> System.out.println(ingredient);
 
         // Arrow (->) represents what happens when it is run
         // No need to specify type for "ingredient" as it knows it will be String
         // ingredients.forEach(ingredient -> System.out.println(ingredient));
 
-        ingredients.forEach(printer);
+        Main.yell("Cupcakes");
+        // ingredients.forEach(System.out::println);
+        ingredients.forEach(Main::yell);
 
     }
 }
